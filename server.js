7e78349f4bd96805a3e53003d6d2852d0a1a2c35
EddieChuang <az564123@gmail.com>
2018-05-24 23:22:08 +0800

    init

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const request = require('request')
+const async = require('async')
+const expressHbs = require('express-handlebars')
+
+const app = express()
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(morgan('dev')) // read requests that user made
+
+app.get('/', (req, res, next) => {
+  res.render('main/home')
+})
+
+app.listen(3030, (err) => {
+  if(err){
+    console.log(err)
+  } else {
+    console.log('Running on Port 3030 ...')
+  }
+})
+
+
+
